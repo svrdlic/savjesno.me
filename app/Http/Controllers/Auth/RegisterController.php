0278380f16d3ac0fbae6c3e1ddab5047faff0192
Ivan Radunovic <ivan@codingo.me>
2016-10-11 23:00:51 +0200

    Removed first and last name from register controller.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,8 +67,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'first_name' => $data['first_name'],
-            'last_name' => $data['last_name'],
+            'first_name' => '',
+            'last_name' => '',
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
